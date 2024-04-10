2c8c62bc96616f18c189b5d9f25bcab6d4f29788
lorenaandradeba <65913534+lorenaandradeba@users.noreply.github.com>
2024-04-10 00:26:58 +0000

    novoooo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Hi Lorena! kkk</h1>
+    return <h1>Hi Lorena! Se a vida estiver muito amarga, dá uma rebolada. Às vezes o açúcar tá no fundo.</h1>
 }
 
 export default Home;
